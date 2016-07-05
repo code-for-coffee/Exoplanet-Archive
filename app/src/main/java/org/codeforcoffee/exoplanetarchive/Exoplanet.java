c5b76d00983170a728cf2f570782e652cfb700ff
code-for-coffee <jtraverjr@gmail.com>
2016-07-05 18:50:04 -0500

    android project init

---
+++ app/src/main/java/org/codeforcoffee/exoplanetarchive/Exoplanet.java
@@ -0,0 +1,14 @@
+package org.codeforcoffee.exoplanetarchive;
+
+/**
+ * Created by codeforcoffee on 7/5/16.
+ * Variables named based off Kepler Confirmed Planet API documentation
+ * http://exoplanetarchive.ipac.caltech.edu/docs/API_kepcandidate_columns.html
+ */
+public class Exoplanet {
+
+    private String kepId; //kepid†	Kepler Identification or KepID
+
+
+
+}
