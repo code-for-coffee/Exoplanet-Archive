fbb2c6427b85503824ca957b726cd8608af211f7
code-for-coffee <jtraverjr@gmail.com>
2016-07-05 19:01:58 -0500

    jupiter mass & other props

--- app/src/main/java/org/codeforcoffee/exoplanetarchive/Exoplanet.java
+++ app/src/main/java/org/codeforcoffee/exoplanetarchive/Exoplanet.java
@@ -1,5 +1,7 @@
 package org.codeforcoffee.exoplanetarchive;
 
+
+
 /**
  * Created by codeforcoffee on 7/5/16.
  * Variables named based off Kepler Confirmed Planet API documentation
@@ -7,8 +9,20 @@ package org.codeforcoffee.exoplanetarchive;
  */
 public class Exoplanet {
 
-    private String kepId; //kepid†	Kepler Identification or KepID
+    private String kepId;       //kepid†	Kepler Identification or KepID
+    private String kepoiName;   //kepoi_name†	KOI Name
+    private String name;        //kepler_name†	Kepler Name
+    private Double radius;      //koi_prad†	Planetary Radius (Earth radii)
+    private Double mass;        // mpl_bmassj†	Planet Mass or M*sin(i) [Jupiter mass]
+    // Best planet mass measurement in units of masses of Jupiter. Either Mass, M*sin(i)/sin(i), or M*sin(i).
+    private String planetType;  //
 
+    public Exoplanet(String kepId, String kepoiName, String name, String radius) {
+        this.kepId = kepId;
+        this.kepoiName = kepoiName;
+        this.name = name;
+        this.radius = Double.valueOf(radius);
+    }
 
 
 }
